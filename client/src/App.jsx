1b75602be24ef350f37a2091bce6d400d978c207
SNEHA BIRADAR <snehabiradar@SNEHAs-MacBook-Air.local>
2024-07-09 17:27:43 +0530

    added the header component and updated routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import Login from "./Pages/Login";
+import Header from "./components/Header";
+
 
 const App = () => {
   return (
-    <div className="text-blue-500">
-      <p>Hello</p>
-    </div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/profile" element={<Profile />}/>
+      <Route path="/about" element={<About />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
